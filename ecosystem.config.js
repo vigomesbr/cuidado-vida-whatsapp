e2224772deb2bf6ef221e165028fbeff4e4d0e09
Vinicius Gomes <viniciusgomes@MacBook-Pro-de-Vinicius.local>
2024-12-01 09:48:58 -0300

    remove enviroment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,12 +7,6 @@ module.exports = {
             instances: 1,  // O número de instâncias (idealmente, 1 em Render devido ao limite de threads)
             autorestart: true,  // Reiniciar automaticamente em caso de falha
             max_memory_restart: '200M',  // Reiniciar o processo caso o uso de memória ultrapasse 200MB
-            env: {
-                NODE_ENV: 'development',  // Definir o ambiente como desenvolvimento por padrão
-            },
-            env_production: {
-                NODE_ENV: 'production',  // Definir o ambiente como produção
-            },
         },
     ],
 };
